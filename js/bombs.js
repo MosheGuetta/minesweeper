54c0f9039fd1b92033a64b5f4db8253c369a070d
MosheGuetta <moshe8guetta@gmail.com>
2024-09-20 15:24:10 +0300

    m

--- js/bombs.js
+++ js/bombs.js
@@ -1,3 +1,46 @@
 'use strict'
 
-var gBombs = []
+function palceMines()
+{
+    let mineCount = gLevel.MINES
+
+    while (mineCount > 0)
+    {
+        const i = getRandomInt(0, Math.sqrt(gBoardSize))
+        const j = getRandomInt(0, Math.sqrt(gBoardSize))
+
+        if(!gBoard[i][j].isMine)
+        {
+            gBoard[i][j].isMine = true
+            mineCount --
+        }
+    }
+}
+
+function setMinesNegsCount()
+{
+    for (var i = 0; i < gBoard.length; i++)
+    {
+        for (var j = 0; j < gBoard.length; j++)
+        {
+            currCell = gBoard[i][j]
+            currCell.negMinesCount = countNegMinesArround(i,j)
+        }
+    }
+}
+
+function countNegMinesArround(cellI, cellJ)
+{
+    let mineCount = 0
+    for(var i = cellI - 1; i<cellI + 1; i++)
+    {
+        for(var j = cellJ - 1; j<= cellJ + 1; j++)
+        {
+            if(i === cellI && j === cellJ) continue
+            if (i < 0 || j < 0 || i >= gBoard.length || j>= gBoard.length) continue
+            if (gBoard[i][j].isMine) mineCount ++
+        }
+    }
+    return mineCount
+}
+
