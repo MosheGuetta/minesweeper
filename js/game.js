8c48a5ce875cc21d77ca17ade32afca0ef03ea37
MosheGuetta <moshe8guetta@gmail.com>
2024-09-21 14:54:09 +0300

    change the time looks

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 
 const TIMER_INTERVAL = 31 // how ofen the timer updataes. with prime number 
 //                              we can see all of the milseconds
-const INITIAL_TIMER_TEXT = '00:00.000' // the way the timer will look like
+const INITIAL_TIMER_TEXT = '00:00' // the way the timer will look like
 
 const EMPTY = ''
 const BOMB = '💣'
@@ -300,9 +300,12 @@ function formatTime(ms)
 {
     var minutes = Math.floor(ms / 60000)
     var seconds = Math.floor((ms % 60000) / 1000)
-    var milliseconds = ms % 1000
+    // var milliseconds = ms % 1000
 
-    return `${padTime(minutes)}:${padTime(seconds)}.${padMiliseconds(milliseconds)}`
+    return `${
+        padTime(minutes)}
+        :${padTime(seconds)}`
+        // .${padMiliseconds(milliseconds)}
 }
 
 
